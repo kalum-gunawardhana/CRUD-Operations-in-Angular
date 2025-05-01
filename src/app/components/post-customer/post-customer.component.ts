e572ed799bd9d46e7337889c62c089a5804dd830
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-01 22:37:47 +0530

    commit

---
+++ src/app/components/post-customer/post-customer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-customer',
+  imports: [],
+  templateUrl: './post-customer.component.html',
+  styleUrl: './post-customer.component.css'
+})
+export class PostCustomerComponent {
+
+}
