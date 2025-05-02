dd9afdcc75ef8a3c4dec54d6924faa6b46f7ed46
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-02 08:23:45 +0530

    commit

--- src/app/customers/customer.service.ts
+++ src/app/customers/customer.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Customer {
+  id?: number;
+  name: string;
+  email: string;
+  address: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  private apiUrl = 'http://localhost:8080/api/customers';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllCustomers():Observable<Customer[]>{
+    return this.http.get<Customer[]>(this.apiUrl);
+  }
+
+  getCustomerById(id: number): Observable<Customer> {
+    return this.http.get<Customer>(`${this.apiUrl}/${id}`);
+  }
+
+  createCustomer(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(this.apiUrl, customer);
+  }
+
+  updateCustomer(id: number, customer: Customer): Observable<Customer> {
+    return this.http.put<Customer>(`${this.apiUrl}/${id}`, customer);
+  }
+
+  deleteCustomer(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
