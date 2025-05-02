4c339d2fb122982508983252d3a8043be00ec73f
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-02 23:13:10 +0530

    commit

--- src/app/components/view-cus/view-cus.component.ts
+++ src/app/components/view-cus/view-cus.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import { CustomerService } from '../../services/customer.service';
+import { Customer } from '../../models/customer.model';
 @Component({
   selector: 'app-view-cus',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './view-cus.component.html',
   styleUrl: './view-cus.component.css'
 })
-export class ViewCusComponent {
+export class ViewCusComponent implements OnInit{
+  displayedColumns: string[] = ['id', 'name', 'email', 'address', 'action'];
+  dataSource: Customer[] = [];
 
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit(): void {
+    this.loadCustomers();
+  }
+
+  // Fetch customers from the API
+  loadCustomers(): void {
+    this.customerService.getCustomers().subscribe((data: Customer[]) => {
+      this.dataSource = data;
+      console.log(this.dataSource);
+    });
+  }
+
+  // Delete a customer
+  deleteCustomer(id: number): void {
+    if (confirm('Are you sure you want to delete this customer?')) {
+      this.customerService.deleteCustomer(id).subscribe(() => {
+        alert('Customer deleted successfully!');
+        this.loadCustomers(); // Reload data after deletion
+      });
+    }
+  }
 }
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Customer } from '../models/customer.model';
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  private url = 'http://localhost:8080/customers';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Fetch all customers
+  getCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(this.url);
+  }
+
+  // Delete customer by ID
+  deleteCustomer(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
 }
