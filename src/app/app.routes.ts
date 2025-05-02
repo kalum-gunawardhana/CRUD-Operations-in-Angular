e0fff56d29ba41bb0b353f2c6a84338257899eeb
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-02 08:01:46 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { PostCustomerComponent } from './components/post-customer/post-customer.component';
 
-export const routes: Routes = [
-    {path: 'post-customer', component: PostCustomerComponent}
-];
+export const routes: Routes = [];
