8a0cfa7def3b14c1ca13ad66a2f46ed5d59627bd
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-02 14:41:34 +0530

    commit

---
+++ src/app/components/save-customer/save-customer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-save-customer',
+  imports: [],
+  templateUrl: './save-customer.component.html',
+  styleUrl: './save-customer.component.css'
+})
+export class SaveCustomerComponent {
+
+}
---
+++ src/app/components/update-customer/update-customer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-customer',
+  imports: [],
+  templateUrl: './update-customer.component.html',
+  styleUrl: './update-customer.component.css'
+})
+export class UpdateCustomerComponent {
+
+}
