18aded2d05b43d01a45aa0f9ff68fd0bd021bd6c
Kalum Gunawardhana <kguna415@gmail.com>
2025-05-02 08:43:03 +0530

    commit

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CustomerListComponent},
+  {path: 'add', component: CustomerFormComponent},
+  {path: 'edit/:id', component: CustomerFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
